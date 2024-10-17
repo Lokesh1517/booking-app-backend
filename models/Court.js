48da67da22ba84f4bda639bb014467de714d950c
Lokesh yadav <97423030+Lokesh1517@users.noreply.github.com>
2024-10-17 13:16:44 +0530

    Update Court.js

--- models/Court.js
+++ models/Court.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
 const CourtSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-    sportId: { type: mongoose.Schema.Types.ObjectId, ref: 'Sport', required: true }
+  name: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Court', CourtSchema);
